f05e7a57faf499bccde103c39840542399702d9b
Diego Villalba <diegovillalba_1805@hotmail.com>
2026-02-28 13:27:56 -0300

    fix(v6.1.151): correccion de version en App.tsx

--- App.tsx
+++ App.tsx
@@ -90,7 +90,7 @@ const App: React.FC = () => {
   }, []);
   // 1. STATE INITIALIZATION
   const [state, setState] = useState<AppState>(() => {
-    const CURRENT_VERSION_ID = 'v6.1.150-APK';
+    const CURRENT_VERSION_ID = 'v6.1.151-APK';
     const SYSTEM_ADMIN_ID = 'b3716a78-fb4f-4918-8c0b-92004e3d63ec';
     const initialAdmin: User = { id: SYSTEM_ADMIN_ID, name: 'Administrador', role: Role.ADMIN, username: 'DDANTE1983', password: '9876543210' };
     const defaultInitialState: AppState = {
